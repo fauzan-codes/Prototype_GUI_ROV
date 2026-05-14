1c7bc243c112c40b64aaed649a2d37a768bc067c
Fauzan <azharfauzan472@gmail.com>
2026-05-14 21:23:29 +0700

    update ui pid, log, and advanced

--- static/script.js
+++ static/script.js
@@ -41,7 +41,7 @@ function initCanvas() {
     const canvas = document.getElementById("trajCanvas");
 
     if (!canvas) {
-        console.warn("Canvas tidak ditemukan ⚠️");
+        console.warn("Canvas tidak ditemukan");
         return;
     }
 
@@ -51,39 +51,32 @@ function initCanvas() {
     let x = 0;
     let y = 0;
 
-    // === SET SIZE DARI CONTAINER ===
     function resizeCanvas() {
         const rect = parent.getBoundingClientRect();
 
         canvas.width = rect.width;
         canvas.height = rect.height;
 
-        // reset posisi ke tengah
         x = canvas.width / 2;
         y = canvas.height / 2;
     }
 
     resizeCanvas();
 
-    // auto resize kalau window berubah
     window.addEventListener("resize", resizeCanvas);
 
     function draw() {
-        // background
         ctx.fillStyle = "#04080f";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        // titik
         ctx.fillStyle = "cyan";
         ctx.beginPath();
         ctx.arc(x, y, 5, 0, Math.PI * 2);
         ctx.fill();
 
-        // gerakan random
         x += (Math.random() - 0.5) * 10;
         y += (Math.random() - 0.5) * 10;
 
-        // biar gak keluar frame
         x = Math.max(0, Math.min(canvas.width, x));
         y = Math.max(0, Math.min(canvas.height, y));
     }
@@ -158,4 +151,47 @@ function initROVImage() {
 
     rovImg.onload = showImage;
     rovImg.onerror = showPlaceholder;
+}
+
+
+// PID
+function sendPID() {
+    const kp = parseFloat(document.getElementById("kp").value).toFixed(4);
+    const ki = parseFloat(document.getElementById("ki").value).toFixed(4);
+    const kd = parseFloat(document.getElementById("kd").value).toFixed(4);
+
+    document.getElementById("kp-last").innerText = "Last: " + kp;
+    document.getElementById("ki-last").innerText = "Last: " + ki;
+    document.getElementById("kd-last").innerText = "Last: " + kd;
+
+    addLog(`[PID] Kp=${kp} Ki=${ki} Kd=${kd}`);
+}
+
+function syncPID(slider, id) {
+    const value = parseFloat(slider.value).toFixed(4);
+    document.getElementById(id).value = value;
+}
+
+function addLog(text) {
+    const box = document.getElementById("logBox");
+
+    const line = document.createElement("div");
+    const time = new Date().toLocaleTimeString();
+
+    line.innerText = `[${time}] ${text}`;
+
+    box.appendChild(line);
+    box.scrollTop = box.scrollHeight;
+}
+
+function clearLog() {
+    document.getElementById("logBox").innerHTML = "";
+}
+
+function copyLog() {
+    navigator.clipboard.writeText(document.getElementById("logBox").innerText);
+}
+
+function fakeLog() {
+    addLog("IMU OK | DEPTH 1.23m | PWM UPDATED");
 }
